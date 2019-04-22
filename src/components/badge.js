48c54a1e58ed0b0277f25a958ec258d0b23b9a3f
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-04-22 10:57:06 -0500

    badge component

---
+++ src/components/badge.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './styles/badge.css';
+
+import imgBadge from '../images/badge-header.svg';
+
+class Badge extends React.Component {
+    render(){
+        return(
+            <div className="Badge">
+                <div className="Badge__header">
+                    <img src={imgBadge} alt="Image Badge"  />
+                </div>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src={this.props.avatarUrl} alt="image avatar" />
+                    <h1>{this.props.firstName} <br /> {this.props.lastName} </h1>
+                </div>
+
+                <div className="Badge__section-info">
+                    <h3>{this.props.jobTitle}</h3>
+                    <div>{this.props.twitter}</div>
+                </div>
+                <div className="Badge__footer">
+                    #PlatziConf
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Badge;
